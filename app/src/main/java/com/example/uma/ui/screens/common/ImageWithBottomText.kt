86dad30fba7c6dae8d6daf596f443f91135908c5
Hai Nguyen <cognation100@gmail.com>
2026-01-07 13:18:53 -0500

    contentscale crop

--- app/src/main/java/com/example/uma/ui/screens/common/ImageWithBottomText.kt
+++ app/src/main/java/com/example/uma/ui/screens/common/ImageWithBottomText.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -43,6 +44,7 @@ fun ImageWithBottomText(
             placeholder = painterResource(R.drawable.specialweek_icon),
             //TODO: Only for preview delete when done
             error = painterResource(R.drawable.specialweek_icon),
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
 
