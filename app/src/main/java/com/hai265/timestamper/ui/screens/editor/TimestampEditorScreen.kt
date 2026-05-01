6078e7f673dd4b4f407976bc383f323dfaf23868
Hai Nguyen <hnguye28@umd.edu>
2026-04-30 17:56:14 -0700

    horizontal padding wiop

--- app/src/main/java/com/hai265/timestamper/ui/screens/editor/TimestampEditorScreen.kt
+++ app/src/main/java/com/hai265/timestamper/ui/screens/editor/TimestampEditorScreen.kt
@@ -16,14 +16,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -69,6 +68,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
@@ -174,21 +174,21 @@ fun TimestampEditorScreen(windowSize: WindowWidthSizeClass) {
             )
         }
         if (windowSize == WindowWidthSizeClass.Medium || windowSize == WindowWidthSizeClass.Expanded) {
+            val layoutDirection = LocalLayoutDirection.current
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Box(
                     modifier = Modifier
+                        .background(Color.Black)
                         .weight(0.7f)
-                        .windowInsetsPadding(WindowInsets(0))
-                        .fillMaxSize()
-                        .background(Color.Black),
+                        .padding(start = innerPadding.calculateLeftPadding(layoutDirection))
+                        .fillMaxSize(),
                     contentAlignment = Alignment.Center
-
                 ) { videoPlayer() }
                 Box(
                     modifier = Modifier
                         .weight(0.3f)
                         .fillMaxSize()
-                        .padding(top = 4.dp, bottom = 4.dp)
+                        .padding(top = 4.dp, bottom = 4.dp, end = innerPadding.calculateEndPadding(layoutDirection))
                 ) { timestampsList(true) }
             }
         } else {
