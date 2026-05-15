bb205b5b2554743d12e32816b9fc04f318e3f22c
Hai Nguyen <hnguye28@umd.edu>
2026-05-14 23:47:01 -0700

    wip

--- app/src/main/java/com/hai265/timestamper/MainActivity.kt
+++ app/src/main/java/com/hai265/timestamper/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.hai265.timestamper
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.fragment.app.FragmentActivity
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.hai265.timestamper.ui.App
 import com.hai265.timestamper.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+private const val TAG = "MainActivity"
+
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
+    val db = Firebase.firestore
+
+    // Create a new user with a first and last name
+    val user = hashMapOf(
+        "first" to "Ada",
+        "last" to "Lovelace",
+        "born" to 1815,
+    )
+
+// Add a new document with a generated ID
+
+
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        val user = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912,
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
 
         enableEdgeToEdge()
 
