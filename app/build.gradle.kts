253ba627cd90742658e48c1bc5c5f5fffbe770cd
Hai Nguyen <hnguye28@umd.edu>
2026-05-20 15:15:58 -0700

    SqlDelightVideosDaoTest powersync driver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -140,6 +140,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.android.driver)
     androidTestImplementation(libs.turbine)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
