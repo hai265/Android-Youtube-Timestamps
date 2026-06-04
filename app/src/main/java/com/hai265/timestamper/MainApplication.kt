1839b6b34e15f3f6e5ab13f04d4ec01be9032a90
Hai Nguyen <cognation100@gmail.com>
2026-06-04 11:38:36 -0400

    connect to powersync

--- app/src/main/java/com/hai265/timestamper/MainApplication.kt
+++ app/src/main/java/com/hai265/timestamper/MainApplication.kt
@@ -6,6 +6,9 @@ import coil3.PlatformContext
 import coil3.SingletonImageLoader
 import coil3.memory.MemoryCache
 import coil3.request.crossfade
+import com.hai265.timestamper.data.database.powersync.SupabaseConnector
+import com.hai265.timestamper.data.repos.AuthRepository
+import com.powersync.PowerSyncDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,6 +16,7 @@ import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -20,6 +24,29 @@ import javax.inject.Singleton
 class MainApplication : Application(), SingletonImageLoader.Factory {
     @Inject
     lateinit var scope: CoroutineScope
+
+    @Inject
+    lateinit var authRepo: AuthRepository
+
+    @Inject
+    lateinit var powerSyncDatabase: PowerSyncDatabase
+
+    @Inject
+    lateinit var supabaseConnector: SupabaseConnector
+
+    override fun onCreate() {
+        super.onCreate()
+        scope.launch {
+            authRepo.userId.collect { userID ->
+                if (userID == null) {
+                    powerSyncDatabase.disconnect()
+                } else {
+                    powerSyncDatabase.connect(supabaseConnector)
+                }
+            }
+        }
+    }
+
     override fun newImageLoader(context: PlatformContext): ImageLoader {
         return ImageLoader.Builder(context)
             .crossfade(true)
