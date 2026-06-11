a82121a595b06394cacfb52f84f96ee7b85fb22b
Hai Nguyen <cognation100@gmail.com>
2026-06-11 13:45:02 -0400

    logger

--- core/data/src/commonMain/kotlin/com/hai265/timestamper/data/repos/TimestampRepository.kt
+++ core/data/src/commonMain/kotlin/com/hai265/timestamper/data/repos/TimestampRepository.kt
@@ -1,9 +1,10 @@
 package com.hai265.timestamper.data.repos
 
-import android.util.Log
+import co.touchlab.kermit.Logger
 import com.hai265.timestamper.data.database.Timestamp
 import com.hai265.timestamper.data.database.TimestampDao
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import kotlin.uuid.Uuid
@@ -17,7 +18,7 @@ class TimestampRepository(
 
     suspend fun addOrUpdateTimestamp(timestamp: Timestamp): Uuid {
         return withContext(Dispatchers.IO) {
-            Log.d("TimestampRepository", "addOrUpdateTimestamp: ")
+            Logger.d(tag = "TimestampRepository") { "addOrUpdateTimestamp: " }
             //Room's upsert returns =-1 if existing timestamp is updated, returns id otherwise
             val timestampId = timestampDao.upsertTimestamp(timestamp)
             timestampId
