54b21926cdeb874229aea3e8024b4da2eae37c18
Hai Nguyen <hnguye28@umd.edu>
2026-07-06 14:01:45 -0400

    version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.hai265.timestamper"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
