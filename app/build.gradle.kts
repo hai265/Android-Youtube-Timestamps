1c98a0f38a1a72ffd13f2393f1a406784958034d
Hai Nguyen <hnguye28@umd.edu>
2026-07-08 10:47:20 -0400

    bump version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.hai265.timestamper"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
